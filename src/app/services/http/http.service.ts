b58e0aa4cb1134153fdb6cc12b57a4ed7f804834
rue-elwahishi <rubawahishi@gmail.com>
2020-02-15 10:59:55 +0100

    designing

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -6,14 +6,15 @@ import { HttpClient } from "@angular/common/http";
 })
 export class HttpService {
   constructor(private http: HttpClient) {}
-  prod:String = "";
-  dev:String = "http://127.0.0.1:8080/api";
-  token = localStorage.getItem('token')
-  headers = {'authorization' : this.token }
-  query = () => `?community=${localStorage.getItem('community')}`
-  get(route, query="") {
-    if(query) query = query +  `&community=${localStorage.getItem('community')}`
-    else query = this.query()
+  prod: String = "";
+  dev: String = "http://127.0.0.1:8080/api";
+  token = localStorage.getItem("token");
+  headers = { authorization: this.token };
+  query = () => `?community=${localStorage.getItem("community")}`;
+  get(route, query = "") {
+    if (query)
+      query = query + `&community=${localStorage.getItem("community")}`;
+    else query = this.query();
     return this.http.get(this.dev + route + query, {
       headers: this.token ? this.headers : {}
     });
@@ -25,6 +26,11 @@ export class HttpService {
   }
   patch(route, json) {
     return this.http.patch(this.dev + route + this.query(), json, {
+      headers: this.token ? this.headers : {}
+    });
+  }
+  delete(route) {
+    return this.http.delete(this.dev + route + this.query(), {
       headers: this.token ? this.headers : {}
     });
   }
