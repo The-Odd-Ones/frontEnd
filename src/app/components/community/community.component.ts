1f436b0fc555a434eb41b2a41d3df9e812924492
rue-elwahishi <rubawahishi@gmail.com>
2020-02-16 18:11:58 +0100

    styling for communitiy component

--- src/app/components/community/community.component.ts
+++ src/app/components/community/community.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "src/app/services/http/http.service";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { DataService } from "src/app/services/data/data.service";
 
 @Component({
-  selector: 'app-community',
-  templateUrl: './community.component.html',
-  styleUrls: ['./community.component.scss']
+  selector: "app-community",
+  templateUrl: "./community.component.html",
+  styleUrls: ["./community.component.scss"]
 })
 export class CommunityComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private http: HttpService, private data: DataService) {}
+  $communities: Observable<Array<Object>>;
   ngOnInit() {
+    this.$communities = this.http
+      .get("/communities")
+      .pipe(map((one: any) => one.result));
   }
+  changeCommunity(community) {
+    localStorage.setItem("community", community.name);
 
+    this.data.Community.next(community.name);
+    location.reload();
+  }
 }
